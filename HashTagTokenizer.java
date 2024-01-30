dd337fb4848ec4e22a40229dc8278da8c5a127dd
Tamir <tamir.gluzman@post.runi.ac.il>
2024-01-30 16:34:49 +0200

    First working version of HashTagTokenizer and SpellChecker

--- HashTagTokenizer.java
+++ HashTagTokenizer.java
@@ -14,26 +14,39 @@ public class HashTagTokenizer {
 
 		In in = new In(fileName);
 
-		// Your code here
+		for (int i = 0; i < dictionary.length; i++) {
+			dictionary[i] = in.readLine();
+		}
 
 		return dictionary;
 	}
 
 	public static boolean existInDictionary(String word, String []dictionary) {
-		// Your code here
+		for (int i = 0; i < dictionary.length; i++) {
+			if (dictionary[i].equals(word)) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	public static void breakHashTag(String hashtag, String[] dictionary) {
+		String hashtagLowerCase = hashtag.toLowerCase();
 
 		// Base case: do nothing (return) if hashtag is an empty string.
-        if (hashtag.isEmpty()) {
+        if (hashtagLowerCase.isEmpty()) {
             return;
         }
  
-        int N = hashtag.length();
+        int N = hashtagLowerCase.length();
 
         for (int i = 1; i <= N; i++) {
-		
+			String substring = hashtagLowerCase.substring(0, i);
+			if (existInDictionary(substring, dictionary)) {
+				System.out.println(substring);
+				breakHashTag(hashtagLowerCase.substring(i, N), dictionary);
+				break;
+			}
         }
     }
 
